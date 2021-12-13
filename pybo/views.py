93a808cce1b2fadd4e401b4686d75e6f8f82225c
baksoyeoung <netantz@naver.com>
2021-12-13 12:42:31 +0900

    21-12-13 1

--- pybo/views.py
+++ pybo/views.py
@@ -9,5 +9,4 @@ def index(request):
     """
     question_list = Question.objects.order_by('-create_date')
     context = {'question_list': question_list}
-    return render(request, 'pybo/question_list.html',context)
-
+    return render(request, 'pybo/question_list.html', context)
