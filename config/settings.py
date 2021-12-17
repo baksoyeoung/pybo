fb01baa9742c58f05704711c58ee98b71fe4a1b7
baksoyeoung <netantz@naver.com>
2021-12-17 18:23:42 +0900

    21-12-17-1

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = '0#&*pm9#n&#gl-nkeq0#ioleu!i4m&&5+34**sb_n4-vsj-u^1'
 DEBUG = True
 
 ALLOWED_HOSTS = ['15.164.63.184']
+# ALLOWED_HOSTS = []
 
 # 로그인 성공후 이동하는 URL
 LOGIN_REDIRECT_URL = '/'
